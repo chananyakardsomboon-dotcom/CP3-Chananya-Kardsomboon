58d6442aef50d3244e6b33d66b147bdb3188ca5c
chananyakardsomboon-dotcom <chananya.kardsomboon@gmail.com>
2026-09-08 16:20:25 +0700

    Add and revised lecture24.py
    revised variable height(deleted cm.) and weight (deleted kg.)

--- lecture24.py
+++ lecture24.py
@@ -3,7 +3,6 @@
 Lecture 24 : การใช้งานตัวแปรในภาษา Python 3 
 Variable คือ ตัวแปรที่ใช้เก็บข้อมูลต่างๆ เป็นเหมือนป้้ายแท็ก
 '''
-
 x=10
 print(x) 
 y=5
@@ -12,17 +11,16 @@ print(y)
 pi=3.14
 print(pi)
 print(pi) 
-
-
+'''
+Homework:สร้างชื่อตัวแปรเก็บ ชื่อ-นามสกุล,อายุ,ส่วนสูง,น้ำหนัก
+'''
 name="Chananya Kardsomboon"
 age="31 year old"
-height= "158 cm."
-weight= "58 kg."
+height= "158"
+weight= "58"
 
 print()
 print(name)
 print(age)
 print(str(height)+" cm.")
 print(str(weight)+ " kg.")
-
-print(type(height))
